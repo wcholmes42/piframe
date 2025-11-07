2474f525698ec0ae841e38995c6cb3aafcd0c91a
Razlex42 <razlex42@gmail.com>
2025-11-06 18:31:00 -0800

    Remove framebuffer clear from init script - was preventing FBI display

--- pi/framebuffer/disable-fbcon.sh
+++ pi/framebuffer/disable-fbcon.sh
@@ -11,11 +11,7 @@ done
 TERM=linux setterm --cursor off --blank 0 > /dev/tty1 2>/dev/null
 printf '\033[?25l' > /dev/tty1 2>/dev/null
 
-# Clear framebuffer to remove boot artifacts
-dd if=/dev/zero of=/dev/fb0 bs=1M count=9 2>/dev/null
+# NO LONGER NEEDED - white bar fixed by removing video parameter from cmdline.txt
+# The init script was clearing the framebuffer which prevented FBI from displaying
 
-# Wait for FBI to start and clear the white bar it creates
-sleep 5
-dd if=/dev/zero of=/dev/fb0 bs=3840 seek=1055 count=25 conv=notrunc 2>/dev/null
-
-echo "Framebuffer console disabled and white bar cleared"
+echo "Framebuffer console disabled"
